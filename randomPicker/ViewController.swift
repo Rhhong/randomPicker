899d92c2aca3414a5b4595e52c1a99d425352981
user131167 <user131167@74-80-239-249.ic2net.net>
2017-12-26 22:13:24 -0800

    Initial Commit

---
+++ randomPicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  randomPicker
+//
+//  Created by user131167 on 12/26/17.
+//  Copyright © 2017 user131167. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
